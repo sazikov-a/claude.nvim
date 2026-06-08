7e16d8df8976efd13014e602e10a194e91aff8ae
Andrey Sazikov <asazikov2017@yandex.ru>
2026-06-08 21:45:14 +0300

    better

--- lua/claude.lua
+++ lua/claude.lua
@@ -10,16 +10,16 @@ function M.setup()
         config = true
     })
 
-    vim.keymap.set("<leader>cc",  "<cmd>ClaudeCode<cr>", { desc = "Toggle Claude" })
-    vim.keymap.set("<leader>ccf", "<cmd>ClaudeCodeFocus<cr>", { desc = "Focus Claude" })
-    vim.keymap.set("<leader>ccr", "<cmd>ClaudeCode --resume<cr>", { desc = "Resume Claude" })
-    vim.keymap.set("<leader>ccC", "<cmd>ClaudeCode --continue<cr>", { desc = "Continue Claude" })
-    vim.keymap.set("<leader>ccm", "<cmd>ClaudeCodeSelectModel<cr>", { desc = "Select Claude model" })
-    vim.keymap.set("<leader>ccb", "<cmd>ClaudeCodeAdd %<cr>", { desc = "Add current buffer" })
-    vim.keymap.set("<leader>ccs", "<cmd>ClaudeCodeSend<cr>", { desc = "Send to Claude" })
-    vim.keymap.set("<leader>ccs", "<cmd>ClaudeCodeTreeAdd<cr>", mode = "v", { desc = "Add file", ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" } })
-    vim.keymap.set("<leader>cca", "<cmd>ClaudeCodeDiffAccept<cr>", { desc = "Acceptt diff" })
-    vim.keymap.set("<leader>ccd", "<cmd>ClaudeCodeDiffDeny<cr>", { desc = "Deny diff" })
+    vim.keymap.set("n", "<leader>cc",  "<cmd>ClaudeCode<cr>", { desc = "Toggle Claude" })
+    vim.keymap.set("n", "<leader>ccf", "<cmd>ClaudeCodeFocus<cr>", { desc = "Focus Claude" })
+    vim.keymap.set("n", "<leader>ccr", "<cmd>ClaudeCode --resume<cr>", { desc = "Resume Claude" })
+    vim.keymap.set("n", "<leader>ccC", "<cmd>ClaudeCode --continue<cr>", { desc = "Continue Claude" })
+    vim.keymap.set("n", "<leader>ccm", "<cmd>ClaudeCodeSelectModel<cr>", { desc = "Select Claude model" })
+    vim.keymap.set("n", "<leader>ccb", "<cmd>ClaudeCodeAdd %<cr>", { desc = "Add current buffer" })
+    vim.keymap.set("n", "<leader>ccs", "<cmd>ClaudeCodeSend<cr>", { desc = "Send to Claude" })
+    vim.keymap.set("v", "<leader>ccs", "<cmd>ClaudeCodeTreeAdd<cr>", { desc = "Add file", ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" } })
+    vim.keymap.set("n", "<leader>cca", "<cmd>ClaudeCodeDiffAccept<cr>", { desc = "Acceptt diff" })
+    vim.keymap.set("n", "<leader>ccd", "<cmd>ClaudeCodeDiffDeny<cr>", { desc = "Deny diff" })
 
 end
 

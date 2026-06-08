51bd5e71a8a762ad03fcd46ef57f844e2ad490a0
Andrey Sazikov <asazikov2017@yandex.ru>
2026-06-08 21:51:57 +0300

    better

--- lua/claude.lua
+++ lua/claude.lua
@@ -1,5 +1,22 @@
 local M = {}
 
+local explorer_filetypes = {
+    "NvimTree",
+    "neo-tree",
+    "oil",
+    "minifiles",
+    "netrw",
+}
+
+local function handle_leader_as()
+    local ft = vim.bo.filetype
+    if vim.tbl_contains(explorer_filetypes, ft) then
+        vim.cmd("ClaudeCodeTreeAdd")
+    else
+        vim.notify("Use visual mode to send selection to Claude", vim.log.levels.INFO)
+    end
+end
+
 function M.setup()
     vim.pack.add({
         { src = "https://github.com/folke/snacks.nvim" },
@@ -17,7 +34,7 @@ function M.setup()
     vim.keymap.set("n", "<leader>ccm", "<cmd>ClaudeCodeSelectModel<cr>", { desc = "Select Claude model" })
     vim.keymap.set("n", "<leader>ccb", "<cmd>ClaudeCodeAdd %<cr>", { desc = "Add current buffer" })
     vim.keymap.set("n", "<leader>ccs", "<cmd>ClaudeCodeSend<cr>", { desc = "Send to Claude" })
-    vim.keymap.set("v", "<leader>ccs", "<cmd>ClaudeCodeTreeAdd<cr>", { desc = "Add file", ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" } })
+    vim.keymap.set("v", "<leader>ccs", handle_leader_as, { desc = "Add file" })
     vim.keymap.set("n", "<leader>cca", "<cmd>ClaudeCodeDiffAccept<cr>", { desc = "Acceptt diff" })
     vim.keymap.set("n", "<leader>ccd", "<cmd>ClaudeCodeDiffDeny<cr>", { desc = "Deny diff" })
 
